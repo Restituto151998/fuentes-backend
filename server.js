79fbe54a66f652653dd5b03f633e1c754c612530
James Lloyd Belda <Jimboy@gmail.com>
2021-04-09 15:46:12 +0800

    ok

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const UserRouter = require('./routes/user.routes');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 
 app.use(cors());
