3b3d4f877874caaf5a592f16b1c01bf670944c50
James Lloyd Belda <Jimboy@gmail.com>
2021-04-09 15:07:15 +0800

    changed updated

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const UserRouter = require('./routes/user.routes');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser : true, useCreateIndex : true, useUnifiedTopology : true});
+
+const connection = mongoose.connection; 
+connection.once('open', () => {
+    console.log('mongoDB connection established.');
+});
+
+app.use('/user', UserRouter);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
